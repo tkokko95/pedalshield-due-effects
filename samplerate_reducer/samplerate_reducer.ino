60bbcdea73c97c43dec4281323c0c521903b8568
tkokko95 <taavi.kokko@gmail.com>
2021-02-21 16:23:38 +0200

    Added sample rate reducer effect

--- samplerate_reducer/samplerate_reducer.ino
+++ samplerate_reducer/samplerate_reducer.ino
@@ -1,19 +1,22 @@
 /*
-A simple bitdepth reducer, resulting in a cool Lo-Fi sound
+A different type of bitcrusher effect, this time by reducing the sample rate.
+Only every n:th sample will "fresh", the previous one will be held and looped in the meantime.
 */
 
-// Setting up vars for ADC/DAC and the potentiometers and the shift value
+// Setting up vars for ADC/DAC, the potentiometer, the loop ratio and the counter
 int ADC0_in, ADC1_in, DAC0_out, DAC1_out;
 int POT0, POT2;
-int shift;
+int n, count = 0;
 
 // Unused constants for the LED, the stomp-switch and the programmable switch
 // Left here for future reference
 /*
 const int LED = 3;
 const int STOMP = 7; 
-const int TGL = 2; 
+const int TOGGLE = 2; 
 */
+
+
 
 void setup() {
     // Setting up the CPU for ADC/DAC
@@ -26,7 +29,6 @@ void setup() {
 }
 
 void loop() {
-
     while((ADC->ADC_ISR & 0x1CC0)!=0x1CC0);// wait for ADC 0, 1, 8, 9, 10 conversion complete.
     ADC0_in=ADC->ADC_CDR[7];               // read data from ADC0
     ADC1_in=ADC->ADC_CDR[6];               // read data from ADC1   
@@ -34,26 +36,20 @@ void loop() {
     //POT1=ADC->ADC_CDR[11];                 // read data from ADC9 (unused, remember to declare if you're going to use this)
     POT2=ADC->ADC_CDR[12];                 // read data from ADC10
 
-    /* 
-    POT0 controls the bitshift amount. Depending on the pot, the bits will be shifted
-    by 0-3 bits.
-    */
-    shift = map(POT0, 0, 4095, 0, 3);
-
-    /*
-    This is where the actual signal processing happens. The bits will be shifted
-    to the left, reducing the bit depth. This will introduce quantization noise
-    to the signal.
-    */
-    ADC0_in = ADC0_in << shift;
-    ADC1_in = ADC1_in << shift;
+    // POT0 controls the number of looped samples
+    n = map(POT0, 0, 4095, 1, 1000);
 
     // POT2 controls the output volume
     DAC0_out = map(ADC0_in, 0, 4095, 1, POT2);
     DAC1_out = map(ADC1_in, 0, 4095, 1, POT2);
 
-    dacc_set_channel_selection(DACC_INTERFACE, 0);          //select DAC channel 0
-    dacc_write_conversion_data(DACC_INTERFACE, DAC0_out);   //write on DAC
-    dacc_set_channel_selection(DACC_INTERFACE, 1);          //select DAC channel 1
-    dacc_write_conversion_data(DACC_INTERFACE, DAC1_out);   //write on DAC
+    // The previous sample will be looped until the counter reaches n
+    while (count < n) {
+        dacc_set_channel_selection(DACC_INTERFACE, 0);          //select DAC channel 0
+        dacc_write_conversion_data(DACC_INTERFACE, DAC0_out);   //write on DAC
+        dacc_set_channel_selection(DACC_INTERFACE, 1);          //select DAC channel 1
+        dacc_write_conversion_data(DACC_INTERFACE, DAC1_out);   //write on DAC
+        count++;
+    }
+    count = 0;
 }
